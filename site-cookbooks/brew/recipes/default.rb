a98d41b315b249dd3d2f770c1a56564bce23de04
hamaco <hamanaka.kazuhiro@gmail.com>
2013-10-21 02:25:49 +0900

    zshとmacvimはoptions必須なので別途インストール

--- site-cookbooks/brew/recipes/default.rb
+++ site-cookbooks/brew/recipes/default.rb
@@ -4,8 +4,22 @@ execute "cleanup homebrew" do
   command "/usr/local/bin/brew cleanup || true"
 end
 
+package "macvim" do
+  options "--HEAD --with-cscope --with-lua --with-luajit"
+  action :install
+end
+
+package "zsh" do
+  options "--disable-etcdir"
+  action :install
+end
+
 node["brew"]["packages"].each do |pkg|
   package pkg do
     action :install
   end
+end
+
+execute "brew linkapps" do
+  command "/usr/local/bin/brew linkapps || true"
 end
