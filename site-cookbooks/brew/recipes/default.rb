c9036144bd1a1cfbf9e7243634357ccc51506cb0
hamaco <hamanaka.kazuhiro@gmail.com>
2013-10-22 18:07:20 +0900

    homebrewで入れたzshがデフォルトシェルになるようにした

--- site-cookbooks/brew/recipes/default.rb
+++ site-cookbooks/brew/recipes/default.rb
@@ -14,6 +14,14 @@ package "zsh" do
   action :install
 end
 
+execute "sudo sh -c \"echo '/usr/local/bin/zsh' >> /etc/shells\"" do
+  not_if "grep /usr/local/bin/zsh /etc/shells"
+end
+
+execute 'sudo dscl . -change /Users/hamaco UserShell $(dscl . -read /Users/hamaco UserShell | sed -e "s/UserShell: //") /usr/local/bin/zsh' do
+  # TODO: パスワード毎回聞かれて面倒なのですでに設定済ならやらない
+end
+
 node["brew"]["packages"].each do |pkg|
   package pkg do
     action :install
