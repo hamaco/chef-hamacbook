49cf4d02bbb0ed79d025c6ea75b61e8559182c23
hamaco <hamanaka.kazuhiro@gmail.com>
2013-11-22 14:09:43 +0900

    一般ユーザ権限だとデフォルトの/var/chefに権限ないのでPATH変更

--- solo.rb
+++ solo.rb
@@ -1,6 +1,7 @@
 cookbook_root = File.expand_path(File.dirname(__FILE__))
 
-file_cache_path "/tmp/chef-solo"
+file_cache_path "/tmp/chef-solo/cache"
+file_backup_path "/tmp/chef-solo/backup"
 # data_bag_path   "/tmp/chef-solo/data_bags"
 # encrypted_data_bag_secret "/tmp/chef-solo/data_bag_key"
 
