5b937c6515d7410d2f0917f2445c85a4c471baa7
hamaco <hamanaka.kazuhiro@gmail.com>
2013-11-29 12:14:02 +0900

    見た目整えた

--- site-cookbooks/macapps/definitions/zip_package.rb
+++ site-cookbooks/macapps/definitions/zip_package.rb
@@ -1,15 +1,16 @@
 define :zip_package do
   remote_file "#{Chef::Config[:file_cache_path]}/#{params[:name]}.zip" do
-    source "#{params[:source]}"
+    source   "#{params[:source]}"
     checksum "#{params[:checksum]}"
-    action :create_if_missing
+    action   :create_if_missing
 
     not_if { ::File.directory?("/Applications/#{params[:name]}.app") }
   end
 
   execute "unzip #{params[:name]}" do
+    cwd     "/Applications"
     command "unzip '#{Chef::Config[:file_cache_path]}/#{params[:name]}.zip'"
-    cwd "/Applications"
+
     not_if { ::File.directory?("/Applications/#{params[:name]}.app") }
   end
 end
