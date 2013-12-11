5ad7254a5c437018d8dbbf505a2eae9d2e3bec91
hamaco <hamanaka.kazuhiro@gmail.com>
2013-12-11 18:56:32 +0900

    update vagrant & virtualbox

--- site-cookbooks/macapps/recipes/vagrant.rb
+++ site-cookbooks/macapps/recipes/vagrant.rb
@@ -1,5 +1,5 @@
 dmg_package "Vagrant" do
-  source "http://files.vagrantup.com/packages/a40522f5fabccb9ddabad03d836e120ff5d14093/Vagrant-1.3.5.dmg"
+  source "https://dl.bintray.com/mitchellh/vagrant/Vagrant-1.4.0.dmg"
   type "pkg"
   package_id "com.vagrant.vagrant"
   action :install
--- site-cookbooks/macapps/recipes/virtualbox.rb
+++ site-cookbooks/macapps/recipes/virtualbox.rb
@@ -1,5 +1,5 @@
 dmg_package "VirtualBox" do
-  source "http://download.virtualbox.org/virtualbox/4.3.0/VirtualBox-4.3.0-89960-OSX.dmg"
+  source "http://download.virtualbox.org/virtualbox/4.3.4/VirtualBox-4.3.4-91027-OSX.dmg"
   type "pkg"
   action :install
 end
